9a858a980dbc195f9fed7f791127ab094ffc71ad
noviceGuru <amin.arabi@gmail.com>
2023-12-16 15:19:16 +0100

    absolute paths enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, "src/components"),
+            assets: path.resolve(__dirname, "src/assets"),
+            features: path.resolve(__dirname, "src/features"),
+            utils: path.resolve(__dirname, "src/utils"),
+        },
+    },
 })
