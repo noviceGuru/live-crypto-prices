2f1a2d4c30a45f5ff085461740e9316f7f9c5e76
noviceGuru <amin.arabi@gmail.com>
2023-12-16 14:46:02 +0100

    Graph shows one price in a window of 10 instances, differentiated in centiseconds

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react"
 import { ResponsiveLine } from "@nivo/line"
 
-type ResponseUnit = {
-    c: Array<number>
-    p: number
-    s: string
-    t: number
-    v: number
-}
-
 type LineChartData = {
     x: string
-    y: number
+    y: number | null
 }
 
 export default function App() {
-    const [data, setData] = useState<LineChartData[]>([])
+    let arr = []
+    for (let i = 0; i < 10; i++) {
+        arr.push({ x: `${i}`, y: null })
+    }
+
+    let rangePush = 5
+
+    const [data, setData] = useState<LineChartData[]>(arr)
+    const [range, setRange] = useState<[number | null, number | null]>([null, null])
 
     useEffect(() => {
         const ws = new WebSocket(`wss://ws.finnhub.io?token=${import.meta.env.VITE_API_KEY}`)
@@ -24,21 +24,52 @@ export default function App() {
             console.log("connection started")
             if (ws.readyState === 1) {
                 // ws.send(JSON.stringify({ type: "subscribe", symbol: "AAPL" }))
-                // ws.send(JSON.stringify({ type: "subscribe", symbol: "BINANCE:BTCUSDT" }))
+                ws.send(JSON.stringify({ type: "subscribe", symbol: "BINANCE:BTCUSDT" }))
                 // ws.send(JSON.stringify({ type: "subscribe", symbol: "IC MARKETS:1" }))
-                ws.send(JSON.stringify({ type: "subscribe", symbol: "AMZN" }))
+                // ws.send(JSON.stringify({ type: "subscribe", symbol: "AMZN" }))
             }
         }
 
         ws.onmessage = event => {
-            console.log(JSON.parse(event.data), 'pure innocent data')
-            const newEntry = JSON.parse(event.data).data.map((e: ResponseUnit) => ({
-                x: `${new Date(e.t).toUTCString()}`,
-                y: e.p,
-            }))
+            // console.log(JSON.parse(event.data), "pure innocent data")
+            let newJson = JSON.parse(event.data).data[0]
+            let newTime = new Date(newJson.t)
+
+            const minutes = newTime.getUTCMinutes()
+            const seconds = newTime.getUTCSeconds()
+            const centiseconds = Math.round(newTime.getUTCMilliseconds() / 10)
+
+            // Use string interpolation to format the result as mm:ss
+            const formattedTime = `${minutes.toString().padStart(2, "0")}
+                :${seconds.toString().padStart(2, "0")}
+                .${centiseconds.toString().padStart(2, '0')}`
+
+            const newEntry: LineChartData = {
+                x: formattedTime,
+                y: JSON.parse(event.data).data[0].p,
+            }
+
+            // INITIALIZATION
+            if (!range[0]) {
+                setRange([(newEntry.y || rangePush) - rangePush, (newEntry.y || 0) + rangePush])
+            }
+
+            // CHECK LOWER LIMIT OF THE RANGE
+            if ((range[0] as number) > newEntry.y!) {
+                setRange(range => [newEntry.y! - rangePush, range[1]])
+            }
+
+            // CHECK UPPER LIMIT OF THE RANGE
+            if ((range[0] as number) < newEntry.y!) {
+                setRange(range => [range[0], newEntry.y! + rangePush])
+            }
+
             setData(oldEntry => {
-                console.log([...oldEntry, ...newEntry])
-                return [...oldEntry, ...newEntry]
+                let oldEntryClone = structuredClone(oldEntry)
+
+                oldEntryClone.shift()
+                let newState = oldEntryClone.concat(newEntry)
+                return newState
             })
         }
 
@@ -51,6 +82,7 @@ export default function App() {
             }
         }
     }, [])
+
     const chartData = [
         {
             id: "Amazon",
@@ -59,22 +91,16 @@ export default function App() {
         },
     ]
 
-    return (
+        return (
         <div className="h-screen p-4">
             <ResponsiveLine
                 data={chartData}
                 margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                xScale={{ type: "point" }}
                 yScale={{
                     type: "linear",
-                    min: "auto",
-                    max: "auto",
-                    stacked: true,
-                    reverse: false,
+                    min: range[0] ?? 0,
+                    max: range[1] ?? rangePush,
                 }}
-                yFormat=" >-.2f"
-                axisTop={null}
-                axisRight={null}
                 axisBottom={{
                     tickSize: 5,
                     tickPadding: 5,
@@ -91,38 +117,6 @@ export default function App() {
                     legendOffset: -40,
                     legendPosition: "middle",
                 }}
-                pointSize={10}
-                pointColor={{ theme: "background" }}
-                pointBorderWidth={2}
-                pointBorderColor={{ from: "serieColor" }}
-                pointLabelYOffset={-12}
-                useMesh
-                legends={[
-                    {
-                        anchor: "bottom-right",
-                        direction: "column",
-                        justify: false,
-                        translateX: 100,
-                        translateY: 0,
-                        itemsSpacing: 0,
-                        itemDirection: "left-to-right",
-                        itemWidth: 80,
-                        itemHeight: 20,
-                        itemOpacity: 0.75,
-                        symbolSize: 12,
-                        symbolShape: "circle",
-                        symbolBorderColor: "rgba(0, 0, 0, .5)",
-                        effects: [
-                            {
-                                on: "hover",
-                                style: {
-                                    itemBackground: "rgba(0, 0, 0, .03)",
-                                    itemOpacity: 1,
-                                },
-                            },
-                        ],
-                    },
-                ]}
             />
         </div>
     )
